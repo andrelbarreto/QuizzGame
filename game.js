262b891993d894c43a70333dbbae9eb53ba57228
Andre Barreto <52931189+andrelbarreto@users.noreply.github.com>
2019-11-18 10:09:47 -0600

    Previous files uploaded  weren't latest  / Timer reset and High Score glitch fix

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
+const TimerText = document.getElementById("timer");
 const progressBarFull = document.getElementById("progressBarFull");
 const loader = document.getElementById("loader");
 const game = document.getElementById("game");
@@ -14,8 +15,8 @@ let availableQuestions = [];
 let questions = [];
 
 fetch(
-  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
-)
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+  )
   .then(res => {
     return res.json();
   })
@@ -44,7 +45,7 @@ fetch(
     startGame();
   })
   .catch(err => {
-    console.error(err);
+    console.error();
   });
 
 //CONSTANTS
@@ -60,11 +61,44 @@ startGame = () => {
   loader.classList.add("hidden");
 };
 
+// Tried a few different approaches on how to add timer to game
+// <script src="https://code.jquery.com/jquery-latest.min.js">
+
+/* let timeleft = 10;
+var downloadTimer = setInterval(function(){
+  document.getElementById("timer").value = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("timer").value = "Finished"
+  }
+}, 1000);  */
+
+
+
+TimerText.value = 60;
+var secondsElapsed = 0;
+
+function getFormattedSeconds() {
+  var secondsLeft = (TimerText.value - secondsElapsed) % 60;
+
+  var formattedSeconds;
+
+  if (secondsLeft < 10) {
+    formattedSeconds = "0" + secondsLeft;
+  } else {
+    formattedSeconds = secondsLeft;
+  }
+
+  return formattedSeconds;
+}
+
+
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     //go to the end page
-    return window.location.assign("/end.html");
+    return window.location.assign("end.html");
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
@@ -74,7 +108,7 @@ getNewQuestion = () => {
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerHTML = currentQuestion.question;
-
+  //checks the answer choice
   choices.forEach(choice => {
     const number = choice.dataset["number"];
     choice.innerHTML = currentQuestion["choice" + number];
@@ -83,32 +117,50 @@ getNewQuestion = () => {
   availableQuestions.splice(questionIndex, 1);
   acceptingAnswers = true;
 };
+//set timer once question is displayed
+setInterval(function () {
+  timer = TimerText.value--;
+  TimerText.innerText = timer;
+  if (timer == 0) {
+    return;
+  }
+  timer = getFormattedSeconds();
+  // check if answer is correct or incorrect and assign points
+  choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+      if (!acceptingAnswers) return;
 
-choices.forEach(choice => {
-  choice.addEventListener("click", e => {
-    if (!acceptingAnswers) return;
+      acceptingAnswers = false;
+      const selectedChoice = e.target;
+      const selectedAnswer = selectedChoice.dataset["number"];
 
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
+      const classToApply =
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-    const classToApply =
-      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+      if (classToApply === "correct") {
+        incrementScore(CORRECT_BONUS);
+      }
 
-    if (classToApply === "correct") {
-      incrementScore(CORRECT_BONUS);
-    }
+      selectedChoice.parentElement.classList.add(classToApply);
 
-    selectedChoice.parentElement.classList.add(classToApply);
 
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
-    }, 1000);
+      // check for time out   
+      setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+        if (timer <= 0) {
+          clearInterval();
+          localStorage.setItem("mostRecentScore", score);
+          //go to the end page
+          return window.location.assign("end.html");
+        }
+      }, 1000);
+    });
   });
-});
 
-incrementScore = num => {
-  score += num;
-  scoreText.innerText = score;
-};
+  // Score counter
+  incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
+}, 1000)
--- highscores.js
+++ highscores.js
@@ -1,8 +1,19 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML = highScores
+/* highScoresList.innerHTML = highScores
   .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
   })
   .join("");
+*/
+  
+  highScoresList.innerHTML = highScores
+    .map(
+      highScore =>
+        `<li class="high-score">${highScore.username} - ${highScore.score}</li>`
+    )
+    .map(score => {
+      return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
+    .join("");
